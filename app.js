5a877db878c25503644456a4825e94c85458050b
ogituyagi <ogituyagi@gmail.com>
2026-09-08 09:05:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -426,12 +426,11 @@ function updateCartUI() {
           <span class="cart-item-title">${item.nama}</span>
           ${item.notes !== "Normal" ? `
             <div class="cart-item-notes">
-              <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
               <span>${item.notes}</span>
             </div>
           ` : ''}
           <button onclick="openCartCustomModal(${idx})" class="btn-custom">
-            ⚙️ Custom
+            <i class="fas fa-pencil-alt"></i> Custom
           </button>
         </div>
 
