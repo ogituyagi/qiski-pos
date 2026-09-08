b36b2a37de123456bc2a2accf8c611bbaaa355b7
ogituyagi <ogituyagi@gmail.com>
2026-09-08 09:16:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -430,7 +430,11 @@ function updateCartUI() {
             </div>
           ` : ''}
           <button onclick="openCartCustomModal(${idx})" class="btn-custom">
-            <i class="fas fa-pencil-alt"></i> Custom
+            <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
+              <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
+            </svg>
+            Custom
           </button>
         </div>
 
